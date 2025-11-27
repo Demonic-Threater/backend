47baa9a589269215b4eb47e1c8a75f427b427439
Dipro Paul <dipropaul82@gmail.com>
2025-11-27 14:40:22 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#!/bin/bash
-export FLASK_APP=app.py
-export FLASK_ENV=production
-
-# Railway automatically injects $PORT as an env variable
-# Use Python to read it safely
-python -m flask run --host=0.0.0.0 --port=${PORT:-8000}
