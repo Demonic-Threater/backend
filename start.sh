b83ff1e84f98b8262c53e70dee418eef6f63d418
Dipro Paul <dipropaul82@gmail.com>
2025-11-27 14:18:11 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+#!/bin/bash
+export FLASK_APP=app.py
+export FLASK_ENV=production
 
-gunicorn app:app --bind 0.0.0.0:$PORT
+# Railway automatically injects $PORT as an env variable
+# Use Python to read it safely
+python -m flask run --host=0.0.0.0 --port=${PORT:-8000}
